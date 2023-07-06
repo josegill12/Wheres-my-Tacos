cd3572f74fec47090c5974bc7c5a0943ce788d13
josegill12 <134568406+josegill12@users.noreply.github.com>
2023-07-05 18:16:08 -0600

    add new code

--- app.js
+++ app.js
@@ -4,11 +4,33 @@ const settings = {
   url: "https://local-business-data.p.rapidapi.com/search-nearby?query=taco%20trucks&lat=39.66146&lng=-104.77723&limit=20&language=en&region=us",
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "759bd9df5emshc146382ccd6b4d0p1fb03ajsn91c47f56cdb0",
-    "X-RapidAPI-Host": "local-business-data.p.rapidapi.com",
+    Authorization: "Bearer API_KEY",
+    accept: application / json,
   },
 };
 
 $.ajax(settings).done(function (response) {
   console.log(response);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch hours of operation
+  fetch("your_hours_api_endpoint")
+    .then((response) => response.json())
+    .then((data) => {
+      const hoursElement = document.getElementById("hours");
+      hoursElement.textContent = data.hours;
+    });
+
+  // Fetch menu items
+  fetch("your_menu_api_endpoint")
+    .then((response) => response.json())
+    .then((data) => {
+      const menuElement = document.getElementById("menu");
+      data.menuItems.forEach((item) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = item;
+        menuElement.appendChild(listItem);
+      });
+    });
 });
