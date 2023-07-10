53cab300c2310211cacf3f183afaa6ae430b0523
josegill12 <134568406+josegill12@users.noreply.github.com>
2023-07-10 11:57:57 -0600

    I finally got it

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const settings = {
-  async: true,
-  crossDomain: true,
-  url: "https://local-business-data.p.rapidapi.com/search?query=taco%20trucks%20in%2080013%2CUSA&limit=20&lat=37.359428&lng=-121.925337&zoom=13&language=en&region=us",
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "759bd9df5emshc146382ccd6b4d0p1fb03ajsn91c47f56cdb0",
@@ -9,6 +6,22 @@ const settings = {
   },
 };
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+function runAPIs() {
+  const api1Url =
+    "https://local-business-data.p.rapidapi.com/search?query=taco%20trucks%20in%2080013%2CUSA&limit=20&lat=37.359428&lng=-121.925337&zoom=13&language=en&region=us";
+  fetch(api1Url, settings)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("API 1 Response:", data);
+    })
+    .catch((error) => {
+      console.error("API 1 Error:", error);
+    });
+}
+
+function handleButtonClick(params) {
+  const truckName = document.getElementById("truckNameInput").value;
+  console.log("Truck Name:", truckName);
+}
+
+runAPIs();
